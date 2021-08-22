e7d9d3d7b4043255abe30c2a1b52a488db1b0d8d
bayardmartins <38463452+bayardmartins@users.noreply.github.com>
2021-08-22 18:57:39 -0300

    estrutura inicial

---
+++ xlab/src/routes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Users from './pages/Users';
+
+export default function Routes () {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Users} />
+            </Switch>
+        </BrowserRouter>       
+    );
+}
